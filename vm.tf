100250e6daee2a9254d3321696312f4c106d7a9a
lakshay-saini <39418344+lakshay-saini@users.noreply.github.com>
2020-02-22 00:10:59 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -17,7 +17,6 @@ resource "azurerm_subnet" "demo" {
   address_prefix       = "10.0.0.0/24"
 }
 
-# Create Public IP
 resource "azurerm_public_ip" "datasourceip" {
   name = "testPublicIp"
   location="${azurerm_resource_group.demo.location}"
@@ -25,7 +24,6 @@ resource "azurerm_public_ip" "datasourceip" {
   public_ip_address_allocation="dynamic"
 }
 
-# Create network interface
 resource "azurerm_network_interface" "myvm_int" {
   name = "myvm_NIC"
   location = "${azurerm_resource_group.demo.location}"
@@ -39,7 +37,6 @@ resource "azurerm_network_interface" "myvm_int" {
    }
 }
 
-# Create a Virtual Machine
 resource "azurerm_virtual_machine" "test" {
     name = "myvm"
     location="${azurerm_resource_group.demo.location}"
@@ -50,41 +47,41 @@ resource "azurerm_virtual_machine" "test" {
     delete_data_disks_on_termination = "true"
 
     storage_image_reference {
-    publisher = "${var.vm_image_publisher}"
-    offer = "${var.vm_image_offer}"
-    sku = "${var.vm_image_sku}"
-    version = "${var.vm_image_version}"
+      publisher = "${var.vm_image_publisher}"
+      offer = "${var.vm_image_offer}"
+      sku = "${var.vm_image_sku}"
+      version = "${var.vm_image_version}"
     }
 
     storage_os_disk {
-    name = "datadisk_new_2018_01"
-    caching = "ReadWrite"
-    create_option = "FromImage"
-    managed_disk_type="Standard_LRS"
-
+      name = "datadisk_new_2018_01"
+      caching = "ReadWrite"
+      create_option = "FromImage"
+      managed_disk_type="Standard_LRS"
     }
+  
     os_profile {
-    computer_name = "SERVER2016"
-    admin_username = "${var.VM_ADMIN}"
-    admin_password = "${var.VM_PASSWORD}"
+      computer_name = "SERVER2016"
+      admin_username = "${var.VM_ADMIN}"
+      admin_password = "${var.VM_PASSWORD}"
     }
 
     os_profile_windows_config {
-    provision_vm_agent = "true"
-    enable_automatic_upgrades = "true"
-    winrm {
-    protocol = "http"
-    certificate_url =""
+      provision_vm_agent = "true"
+      enable_automatic_upgrades = "true"
+      winrm {
+        protocol = "http"
+        certificate_url =""
       }
    }
 }
 
-#Retrieve public IP
 data "azurerm_public_ip" "test" {
   name = "${azurerm_public_ip.datasourceip.name}"
   resource_group_name = "${azurerm_resource_group.demo.name}"
   depends_on = ["azurerm_virtual_machine.test"]
 }
+
 output "public_ip_address" {
   value = "${data.azurerm_public_ip.test.ip_address}"
 }
